d1cc49ce7935d8b91185537386b78a5516693082
lys1313013 <lys1313013@gmail.com>
2025-04-06 19:10:55 +0800

    feat: 除登录接口外，所有接口都需要登录鉴权

--- src/main/java/com/lys/config/SaTokenConfig.java
+++ src/main/java/com/lys/config/SaTokenConfig.java
@@ -1,12 +1,19 @@
 package com.lys.config;
 
+import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.jwt.StpLogicJwtForStateless;
 import cn.dev33.satoken.stp.StpLogic;
+import cn.dev33.satoken.stp.StpUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class SaTokenConfig {
+public class SaTokenConfig implements WebMvcConfigurer {
 
     /**
      * 配置 StpLogic 使用 JWT 无状态模式
@@ -15,5 +22,13 @@ public class SaTokenConfig {
     @Bean
     public StpLogic getStpLogicJwt() {
         return new StpLogicJwtForStateless();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
+        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
+                .addPathPatterns("/**")
+                .excludePathPatterns("/auth/login");
     }
 }
