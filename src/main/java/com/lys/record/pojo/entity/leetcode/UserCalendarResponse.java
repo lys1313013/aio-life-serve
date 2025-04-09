06e590b736a5394c30c39de9d497f25a9edb7fcc
lys1313013 <lys1313013@gmail.com>
2025-04-09 23:49:34 +0800

    feat: leetcode热力图接口返回值

---
+++ src/main/java/com/lys/record/pojo/entity/leetcode/UserCalendarResponse.java
@@ -0,0 +1,34 @@
+package com.lys.record.pojo.entity.leetcode;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+/**
+ * leetcode热力图接口返回值
+ *
+ * @author Lys
+ * @date 2025/04/09 23:45
+ */
+@Getter
+@Setter
+public class UserCalendarResponse {
+    private Data data;
+
+    @Getter
+    @Setter
+    public static class Data {
+        private UserCalendar userCalendar;
+    }
+
+    @Getter
+    @Setter
+    public static class UserCalendar {
+        private int streak;
+        private int totalActiveDays;
+        private String submissionCalendar;
+        private List<Integer> activeYears;
+        private Object monthlyMedals; // 根据实际情况可替换为具体类型
+        private int recentStreak;
+    }
+}
