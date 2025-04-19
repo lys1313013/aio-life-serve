c41d584d0d9c329ef3924a30697d8c79cbab46e1
lys1313013 <lys1313013@gmail.com>
2025-04-19 10:54:53 +0800

    feat: 增加消息通知VO

---
+++ src/main/java/com/lys/record/pojo/vo/NotificationVo.java
@@ -0,0 +1,35 @@
+package com.lys.record.pojo.vo;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 消息通知VO
+ *
+ * @author Lys
+ * @date 2025/04/19 10:52
+ */
+@Data
+public class NotificationVo {
+
+    /**
+     * 头像url
+     */
+    private String avatar;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 消息
+     */
+    private String message;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+}
