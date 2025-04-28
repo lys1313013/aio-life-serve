34f17d0f8c1acf82857d159335d380c67c1bec56
lys1313013 <lys1313013@gmail.com>
2025-04-28 22:52:48 +0800

    feat: 增加统计信息查询接口

--- src/main/java/com/lys/record/api/DashboardController.java
+++ src/main/java/com/lys/record/api/DashboardController.java
@@ -3,6 +3,7 @@ package com.lys.record.api;
 import cn.dev33.satoken.stp.StpUtil;
 import com.lys.core.resq.ApiResponse;
 import com.lys.record.pojo.vo.DashboardCardVO;
+import com.lys.record.pojo.vo.StaticVO;
 import com.lys.record.service.ILeetcodeService;
 import com.lys.sso.mapper.UserMapper;
 import com.lys.sso.pojo.entity.UserEntity;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,6 +32,9 @@ public class DashboardController {
     private UserMapper userMapper;
 
 
+    /**
+     * 看板卡片
+     */
     @PostMapping("/card")
     public ApiResponse<List<DashboardCardVO>> card() {
         int userId = StpUtil.getLoginIdAsInt();
@@ -40,4 +47,24 @@ public class DashboardController {
         return ApiResponse.success(dashboardCard);
     }
 
+    /**
+     * 统计信息
+     */
+    @PostMapping("/static")
+    public ApiResponse<StaticVO> staticInfo() {
+        int userId = StpUtil.getLoginIdAsInt();
+
+        List<StaticVO> voList = new ArrayList<>();
+        StaticVO staticVO = new StaticVO();
+        staticVO.setName("今年已过");
+
+        // 计算今年已过的天数
+        LocalDate today = LocalDate.now();
+        LocalDate firstDayOfYear = LocalDate.of(today.getYear(), 1, 1);
+        long daysPassed = ChronoUnit.DAYS.between(firstDayOfYear, today) + 1;
+        staticVO.setValue((int) daysPassed);
+
+        voList.add(staticVO);
+        return ApiResponse.success(staticVO);
+    }
 }
---
+++ src/main/java/com/lys/record/api/GitHubController.java
@@ -0,0 +1,10 @@
+package com.lys.record.api;
+
+/**
+ * 类功能描述
+ *
+ * @author Lys
+ * @date 2025/04/28 22:15
+ */
+public class GitHubController {
+}
