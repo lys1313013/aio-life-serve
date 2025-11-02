802c6abf09cb43a88b5434fa5aa612d40c077696
lys1313013 <lys1313013@gmail.com>
2025-11-02 16:21:08 +0800

    feat(env): 添加环境变量获取工具类

---
+++ src/test/java/com/lys/GetEnv.java
@@ -0,0 +1,11 @@
+package com.lys;
+
+public class GetEnv {
+    public static void main(String[] args) {
+        System.out.println(System.getenv("AIO_LIFE_DB_PASSWORD"));
+        System.out.println(System.getenv("AIO_LIFE_DB_URL"));
+        System.out.println(System.getenv("AIO_LIFE_SERVER_IP"));
+        System.out.println(System.getenv("AIO_LIFE_SERVER_USER"));
+        System.out.println(System.getenv("AIO_LIFE_SERVER_PASSWORD"));
+    }
+}
