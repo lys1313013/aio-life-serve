f856d3272e72359bfe26d6a8673bc77d7ba1f470
lys1313013 <lys1313013@gmail.com>
2025-11-16 20:39:01 +0800

    feat(record): 新增想法管理模块

---
+++ src/main/java/com/lys/record/mapper/IThoughtMapper.java
@@ -0,0 +1,13 @@
+package com.lys.record.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lys.record.pojo.entity.ThoughtEntity;
+
+/**
+ * 类功能描述
+ *
+ * @author Lys
+ * @date 2025-11-16 16:58
+ */
+public interface IThoughtMapper extends BaseMapper<ThoughtEntity> {
+}
