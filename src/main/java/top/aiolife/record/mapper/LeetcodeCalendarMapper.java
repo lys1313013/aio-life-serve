c8e3e4b7c68df7abacc29a22170888c093eaf75e
lys1313013 <lys1313013@gmail.com>
2026-03-01 14:31:23 +0800

    refactor: 删除无用类

--- src/main/java/top/aiolife/record/mapper/LeetcodeCalendarMapper.java
+++
@@ -1,8 +0,0 @@
-package top.aiolife.record.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import top.aiolife.record.pojo.entity.LeetcodeCalendarEntity;
-
-public interface LeetcodeCalendarMapper extends BaseMapper<LeetcodeCalendarEntity> {
-}
