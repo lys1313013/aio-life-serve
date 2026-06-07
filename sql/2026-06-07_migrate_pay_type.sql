9783cc3d48b1bcd0e74abe280549823db2a32111
lys1313013 <lys1313013@gmail.com>
2026-06-07 18:23:28 +0800

    fix(expense): update pay type migration sql script

--- sql/2026-06-07_migrate_pay_type.sql
+++ sql/2026-06-07_migrate_pay_type.sql
@@ -13,12 +13,6 @@ VALUES
 (0, 'pay_type', 6, '招商信号卡', '6', '0', 0, 0),
 (0, 'pay_type', 7, '京东', '7', '0', 0, 0);
 
--- 3. 迁移 expense 表的数据
--- 我们将 pay_type_id 更新为 user_dict_data 表中对应的 id
-UPDATE expense e
-JOIN user_dict_data udd ON e.pay_type_id = udd.dict_value AND udd.dict_type = 'pay_type' AND udd.user_id = 0
-SET e.pay_type_id = udd.id
-WHERE e.pay_type_id IN (1, 2, 3, 4, 5, 6, 7);
 
--- 4. 修改 expense 表的 pay_type_id 类型为 varchar(32) 或者 bigint
-ALTER TABLE `expense` MODIFY COLUMN `pay_type_id` VARCHAR(32) COMMENT '支付方式ID(关联user_dict_data)';
+-- 4. 修改 expense 表的 pay_type_id 类型为 bigint
+ALTER TABLE `expense` MODIFY COLUMN `pay_type_id` bigint COMMENT '支付方式ID(关联user_dict_data)';
